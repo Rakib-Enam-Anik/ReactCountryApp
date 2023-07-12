b27eb2930f0bc998aa328563c3bd4db6bcc1d9b2
Rakib Enam <md.rakib.enam.anik@bracu.ac.bd>
2023-07-12 12:50:50 +0600

    Country app file added

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ const App = () => {
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState(null);
   const [countries, setCountries] = useState([]);
-  const [filteredCountries, setFilteredCountries] = useState([]);
+  const [filteredCountries, setFilteredCountries] = useState([countries]);
 
   const fetchData = async (url) => {
     setIsLoading(true);
@@ -20,6 +20,7 @@ try{
   const response = await fetch(url);
   const data = await response.json();
   setCountries(data);
+  setFilteredCountries(data);
   setIsLoading(false);
   setError(null);
   
@@ -36,13 +37,30 @@ try{
   }, []);
 
   const handleRemoveCountry = (name) => {
-    props.onRemoveCountry(name);
-  }
+    const filter = filteredCountries.filter((country)
+    => country.name.common !== name);
+    setFilteredCountries(filter) 
+  };
 
-  return ( <div>Country App 
+  const handleSearch = (searchValue) => {
+    let value = searchValue.tolowerCase();
+    const newCountries = countries.filter((country)=>{
+      const countryName = country.name.toLowerCase();
+      return countryName.startsWith(value)
+    });
+    setFilteredCountries(newCountries);
+  };
+
+  const handleRemoveCountry = (name) => {
+    props.onRemoveCountry(name);
+  };
+
+  return ( <div>
+    <h1>Country App </h1>
+    <Search onSearch={handleSearch}/>
   {isLoading && <h2> Loading... </h2>}
   {error && <h2> { error.message } </h2>}
-  {countries && <Countries countries={countries}
+  {countries && <Countries countries={filteredCountries}
   onRemoveCountry={handleRemoveCountry}/>}
 
 
